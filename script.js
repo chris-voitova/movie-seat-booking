2e687bcfc794bf819ec9ee4beea1a625355d36f2
Chris Voitova <voitova@teamharbour.com>
2021-03-05 22:09:54 +0200

    update code style

--- script.js
+++ script.js
@@ -4,8 +4,31 @@ const MOVIE_RESULT_CONTAINER = document.querySelector(".movie");
 const TIME_RESULT_CONTAINER = document.querySelector(".time");
 const SEATS_RESULT_CONTAINER = document.querySelector(".seats");
 const HALL_ITEMS = document.querySelectorAll(".hall-item__input");
-const HALL_ITEM = document.querySelectorAll(".hall-item");
 let activeSeatsList = [];
+
+const removeActiveSeats = () => {
+  HALL_ITEMS.forEach((item) => {
+    item.checked = false;
+  });
+};
+const changeSeatsState = (isDisabled) => {
+  HALL_ITEMS.forEach((item) => {
+    item.disabled = isDisabled;
+  });
+};
+const displayDefaultResult = (resultField) => {
+  resultField.innerHTML = "-";
+};
+
+const handleMovieExists = () => {
+  if (MOVIE_SELECT.value && TIME_SELECT.value) {
+    const isSeatsDisabled = false;
+    changeSeatsState(isSeatsDisabled);
+  } else {
+    const isSeatsDisabled = true;
+    changeSeatsState(isSeatsDisabled);
+  }
+};
 
 const displayResultFromSelect = (selectElement, resultField) => {
   selectElement.addEventListener("change", (event) => {
@@ -18,41 +41,6 @@ const displayResultFromSelect = (selectElement, resultField) => {
       displayDefaultResult(resultField);
     } else {
       resultField.innerHTML = `${selectValue}`;
-    }
-  });
-};
-
-const removeActiveSeats = () => {
-  HALL_ITEMS.forEach((item) => (item.checked = false));
-};
-const changeSeatsState = (isDisabled) => {
-  HALL_ITEMS.forEach((item) => (item.disabled = isDisabled));
-};
-const displayDefaultResult = (resultField) => (resultField.innerHTML = "-");
-
-const handleMovieExists = () => {
-  if (MOVIE_SELECT.value && TIME_SELECT.value) {
-    const isSeatsDisabled = false;
-    changeSeatsState(isSeatsDisabled);
-  } else {
-    const isSeatsDisabled = true;
-    changeSeatsState(isSeatsDisabled);
-  }
-};
-
-const handleSeatsResult = () => {
-  const hall = document.querySelector(".hall");
-  hall.addEventListener("change", (event) => {
-    const target = event.target;
-    const seatValue = target.value;
-    if (target.classList.contains("hall-item__input")) {
-      if (!activeSeatsList.includes(seatValue)) {
-        activeSeatsList.push(seatValue);
-        displaySeatsResult(activeSeatsList);
-      } else {
-        activeSeatsList.splice(activeSeatsList.indexOf(seatValue), 1);
-        displaySeatsResult(activeSeatsList);
-      }
     }
   });
 };
@@ -73,6 +61,22 @@ const displaySeatsResult = (resultData) => {
   } else {
     displayDefaultResult(SEATS_RESULT_CONTAINER);
   }
+};
+
+const handleSeatsResult = () => {
+  const hall = document.querySelector(".hall");
+  hall.addEventListener("change", ({ target }) => {
+    const seatValue = target.value;
+    if (target.classList.contains("hall-item__input")) {
+      if (!activeSeatsList.includes(seatValue)) {
+        activeSeatsList.push(seatValue);
+        displaySeatsResult(activeSeatsList);
+      } else {
+        activeSeatsList.splice(activeSeatsList.indexOf(seatValue), 1);
+        displaySeatsResult(activeSeatsList);
+      }
+    }
+  });
 };
 
 handleSeatsResult(SEATS_RESULT_CONTAINER);
